10765ec4310441eb8c3c362510a038257f423e9d
m.hirosawa <m.hirosawa0228@gmail.com>
2021-01-17 23:09:37 +0900

    addlink&siteTitle

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'まさゆき'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Masayuki Blog'
 
 export default function Layout({ children, home }) {
   return (
